e120d627ba20bc38c5f97625ac25b188471c36f8
Dheeraj Sharma <dsprofessional12@gmail.com>
2023-03-03 16:39:49 +0530

    Ready For Deployment

---
+++ js/index.js
@@ -0,0 +1,7 @@
+hamburger.addEventListener("click", () => {
+    sideBar.classList.add("activeSideBar");
+});
+
+closeSidebar.addEventListener("click", () => {
+    sideBar.classList.remove("activeSideBar");
+});
